50e5d3fe93a7a2b3ac46d04c4cee5d9336e5ff11
Dandav2923 <daniele.emili23@gmail.com>
2022-02-10 16:03:53 +0100

    bonus

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/pagina-1', function () {
+    return view('pagina-1');
+})-> name("pagina-1");
+Route::get('/pagina-2', function () {
+    return view('pagina-2');
+})->name("pagina-2");
